e1655374b89169d4d081f61d66090c565892ff2c
Kotlin-Chan <developer.hazuku@gmail.com>
2021-04-08 05:49:58 +0900

    Compatible with 1.16 bukkit

--- src/main/kotlin/kotx/minecraft/libs/flylib/Extensions.kt
+++ src/main/kotlin/kotx/minecraft/libs/flylib/Extensions.kt
@@ -6,12 +6,19 @@
 package kotx.minecraft.libs.flylib
 
 import kotx.minecraft.libs.flylib.command.Command
-import net.md_5.bungee.api.chat.ComponentBuilder
+import net.kyori.adventure.audience.MessageType
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.format.TextColor
 import org.bukkit.command.CommandSender
+import java.awt.Color
 
 operator fun List<Command>.get(query: String) =
     find { it.name.equals(query, true) } ?: find { it.aliases.any { it == query } }
 
-fun CommandSender.send(block: ComponentBuilder.() -> Unit) {
-    spigot().sendMessage(*ComponentBuilder().apply(block).create())
+fun CommandSender.send(block: TextComponent.Builder.() -> Unit) {
+    sendMessage(Component.text().apply(block).build(), MessageType.CHAT)
 }
+
+fun TextComponent.Builder.append(text: String) = append(Component.text(text))
+fun TextComponent.Builder.color(color: Color) = color(TextColor.color(color.rgb))
