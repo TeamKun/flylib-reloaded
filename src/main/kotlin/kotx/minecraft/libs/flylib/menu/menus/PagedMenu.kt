a85ed11a85a5374496d473b01e7855a08a4c36d0
Kotlin-Chan <developer.hazuku@gmail.com>
2021-04-11 18:51:44 +0900

    add PagedMenu.kt

---
+++ src/main/kotlin/kotx/minecraft/libs/flylib/menu/menus/PagedMenu.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package kotx.minecraft.libs.flylib.menu.menus
+
+import kotx.minecraft.libs.flylib.menu.Menu
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.koin.core.component.KoinComponent
+
+class PagedMenu(
+    player: Player,
+    size: Int,
+    items: MutableList<MenuItem>
+) : Menu(player, size, items), KoinComponent {
+    override fun display() {
+        items.forEach {
+            inventory.setItem(it.index, it.stack)
+        }
+        player.openInventory(inventory)
+    }
+
+    override fun onClick(event: InventoryClickEvent) {
+        event.isCancelled = true
+
+        items.firstOrNull { it.index == event.slot }?.also { it.onClick(event) }
+    }
+
+    class Builder(player: Player) : Menu.Builder<ChestMenu>(player) {
+        override fun build(): ChestMenu = ChestMenu(player, size, items)
+    }
+
+    companion object {
+        fun create(player: Player, block: Builder.() -> Unit) {
+            Builder(player).apply(block).build().display()
+        }
+    }
+}
