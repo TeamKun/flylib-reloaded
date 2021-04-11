592534d358444f7702d4c94c0e2c26a5aa9d53e3
Kotlin-Chan <developer.hazuku@gmail.com>
2021-04-11 10:18:57 +0900

    Move usageReplacements to child elements of UsageCompletionContributor

--- src/main/kotlin/kotx/minecraft/libs/flylib/command/complete/providers/UsageCompletionContributor.kt
+++ src/main/kotlin/kotx/minecraft/libs/flylib/command/complete/providers/UsageCompletionContributor.kt
@@ -8,7 +8,10 @@ package kotx.minecraft.libs.flylib.command.complete.providers
 import kotx.minecraft.libs.flylib.command.CommandContext
 import kotx.minecraft.libs.flylib.command.complete.CompletionContributor
 
-class UsageCompletionContributor : CompletionContributor() {
+class UsageCompletionContributor(
+    private val onlyCompleteIfEmpty: Boolean = true,
+    private val usageReplacements: Map<(String) -> Boolean, CommandContext.() -> List<String>> = emptyMap()
+) : CompletionContributor() {
     override fun suggest(context: CommandContext): List<String> {
         return context.command.usages.mapNotNull {
             it.context.split(" ").getOrNull(context.args.size)
@@ -25,18 +28,18 @@ class UsageCompletionContributor : CompletionContributor() {
                     listOf("...")
                 }
                 templateContentSplit?.size ?: 0 == 1 -> {
-                    val replacements = commandHandler.usageReplacements
+                    val replacements = usageReplacements
                         .filter { it.key(templateResult!!.groupValues[1]) }
                         .flatMap { it.value.invoke(context) }
 
                     if (replacements.isEmpty()) listOf(it) else replacements
                 }
                 templateContentSplit?.size ?: 0 > 1 -> templateContentSplit!!.flatMap { s ->
-                    val replacements = commandHandler.usageReplacements
+                    val replacements = usageReplacements
                         .filter { it.key(s) }
                         .flatMap { it.value.invoke(context) }
 
-                    if (replacements.isNotEmpty()) replacements else listOf(s)
+                    replacements.ifEmpty { listOf(s) }
                 }
                 else -> listOf(it)
             }
@@ -46,12 +49,16 @@ class UsageCompletionContributor : CompletionContributor() {
     override fun postProcess(
         currentCompletion: List<String>,
         context: CommandContext
-    ): List<String> = if (currentCompletion.isEmpty())
+    ): List<String> = if (!onlyCompleteIfEmpty || (onlyCompleteIfEmpty && currentCompletion.isNotEmpty())) {
         currentCompletion.filter {
-            if (context.args.lastOrNull()
-                    .isNullOrBlank()
-            ) true else !it.matches("<.+>|\\[.+]|\\(.+\\)|\\.\\.\\.".toRegex())
-        }.filter { it.startsWith(context.args.lastOrNull() ?: "", true) }
-    else
-        currentCompletion.filter { !it.matches("<.+>|\\[.+]|\\(.+\\)|\\.\\.\\.".toRegex()) }
+            if (context.args.lastOrNull().isNullOrBlank())
+                true
+            else
+                !it.matches("<.+>|\\[.+]|\\(.+\\)|\\.\\.\\.".toRegex())
+        }.filter {
+            it.startsWith(context.args.lastOrNull() ?: "", true)
+        }
+    } else currentCompletion.filterNot {
+        it.matches("<.+>|\\[.+]|\\(.+\\)|\\.\\.\\.".toRegex())
+    }
 }
