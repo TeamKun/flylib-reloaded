24a8ae4e5f1939787674255fea8b972a0409944c
Kotlin-Chan <developer.hazuku@gmail.com>
2021-04-17 00:05:51 +0900

    renamee

--- TestServer/src/main/kotlin/kotx/minecraft/test/KPluginTest.kt
+++ TestServer/src/main/kotlin/kotx/minecraft/test/KPluginTest.kt
@@ -18,9 +18,9 @@ import org.bukkit.plugin.java.JavaPlugin
 class PluginTest : JavaPlugin() {
     private val flyLib = flyLib {
         command {
-            register(TestCommand())
+            register(KTestCommand())
 
-            default {
+            defaultConfiguration {
                 description("this is a description of the default command.")
                 permission(Permission.EVERYONE)
                 invalidMessage { "Hey! Looks like you don't have the necessary permissions to run the command!" }
@@ -42,7 +42,7 @@ class PluginTest : JavaPlugin() {
     }
 }
 
-class TestCommand : Command("test") {
+class KTestCommand : Command("test") {
     override fun CommandContext.execute() {
         if (args.isEmpty()) {
             sendHelp()
