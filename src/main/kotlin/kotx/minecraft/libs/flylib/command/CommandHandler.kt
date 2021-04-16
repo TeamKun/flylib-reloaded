435157744c4bb36970749f09adb98e5e76576a9c
Kotlin-Chan <developer.hazuku@gmail.com>
2021-04-16 23:57:34 +0900

    moved params into defaultCommand

--- src/main/kotlin/kotx/minecraft/libs/flylib/command/CommandHandler.kt
+++ src/main/kotlin/kotx/minecraft/libs/flylib/command/CommandHandler.kt
@@ -5,26 +5,21 @@
 
 package kotx.minecraft.libs.flylib.command
 
-import kotx.minecraft.libs.flylib.*
 import kotx.minecraft.libs.flylib.command.internal.CommandCompletion
+import kotx.minecraft.libs.flylib.command.internal.CommandDefault
 import kotx.minecraft.libs.flylib.command.internal.Permission
-import net.kyori.adventure.text.format.TextDecoration
+import kotx.minecraft.libs.flylib.get
 import org.bukkit.command.CommandSender
 import org.bukkit.permissions.PermissionDefault
 import org.bukkit.plugin.java.JavaPlugin
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.slf4j.Logger
-import java.awt.Color
 
 class CommandHandler(
     private val commands: List<Command>,
     val commandCompletion: CommandCompletion,
-    val defaultDescription: String,
-    val defaultPermission: Permission,
-    val defaultPlayerOnly: Boolean,
-    val invalidCommandMessage: (Command) -> String,
-    val defaultSendHelp: CommandContext.() -> Unit
+    val commandDefault: CommandDefault
 ) : KoinComponent {
     private val plugin: JavaPlugin by inject()
     private val logger: Logger by inject()
@@ -101,133 +96,7 @@ class CommandHandler(
         private val commands = mutableListOf<Command>()
 
         private var commandCompletion: CommandCompletion = CommandCompletion.Builder().build()
-
-        /**
-         * Description specified by default when nothing is specified when creating a Command instance
-         */
-        private var defaultDescription: String = ""
-
-        /**
-         * Privileges specified by default if nothing is specified when creating a command instance
-         */
-        private var defaultPermission: Permission = Permission.OP
-
-
-        /**
-         * Whether or not only the player specified by default can be executed if nothing is specified when creating a command instance
-         */
-        private var defaultPlayerOnly: Boolean = false
-
-        /**
-         * Message sent when the command is unavailable
-         */
-        private var invalidCommandMessage: (Command) -> String = {
-            "You can't execute ${it.name} command!"
-        }
-
-        /**
-         * Contents of send Help used by default when not overridden
-         */
-        private var defaultSendHelp: CommandContext.() -> Unit = {
-            var fullName = command.name
-            fun Command.getFullName() {
-                if (parent != null) {
-                    fullName = "${parent!!.name} $fullName"
-                    parent!!.getFullName()
-                }
-            }
-            command.getFullName()
-
-            sender.send {
-                val nameColor = Color.RED
-                val mainColor = Color.CYAN
-                val subColor = Color.LIGHT_GRAY
-                val highlightTextColor = Color.ORANGE
-                append("-----------------------------------\n", Color.DARK_GRAY)
-                append("/$fullName", nameColor)
-                if (command.aliases.isNotEmpty()) {
-                    append("(", Color.WHITE)
-                    command.aliases.forEachIndexed { i, it ->
-                        append(it, mainColor)
-                        if (i < command.aliases.size - 1)
-                            append(", ", subColor)
-                    }
-                    append(")", Color.WHITE)
-                }
-
-                if (command.description.isNotEmpty()) {
-                    append(" - ", subColor)
-                    append(command.description, subColor)
-                }
-
-                append("\n")
-
-                fun Command.handleParent(current: String): String = if (parent != null)
-                    parent!!.handleParent("${parent!!.name} $current")
-                else
-                    "/$current"
-
-                when (command.usages.size) {
-                    0 -> {
-                    }
-                    1 -> {
-                        val usage = command.usages.first()
-
-                        append("Usage: ", Color.WHITE, decoration = TextDecoration.BOLD)
-                        append(command.handleParent(usage.context), mainColor)
-                        if (usage.description.isNotEmpty()) {
-                            append(" - ", subColor)
-                            append(usage.description, subColor)
-                        }
-
-                        append("\n")
-                    }
-                    else -> {
-                        appendln()
-                        append("# ", Color.WHITE)
-                        append("Usages:\n", highlightTextColor, decoration = TextDecoration.BOLD)
-
-                        command.usages.associate { command.handleParent(it.context) to it.description }.forEach { (usage, description) ->
-                            append(usage, mainColor)
-                            if (description.isNotEmpty()) {
-                                append(" - ", subColor)
-                                append(description, subColor)
-                            }
-                            append("\n")
-                        }
-                    }
-                }
-
-                when (command.examples.size) {
-                    0 -> {
-
-                    }
-
-                    1 -> {
-                        append("Example: ", Color.WHITE, decoration = TextDecoration.BOLD)
-                        append("/${command.examples.first()}\n", mainColor)
-                    }
-
-                    else -> {
-                        appendln()
-                        append("# ", Color.WHITE)
-                        append("Examples:\n", highlightTextColor, decoration = TextDecoration.BOLD)
-                        command.examples.map { "/$it".asTextComponent(mainColor) }.joint("\n".asTextComponent()) {
-                            append(it)
-                        }
-                    }
-                }
-
-                if (command.children.isNotEmpty()) {
-                    command.children.map { "    ${it.name}".asTextComponent(highlightTextColor) }.joint("\n".asTextComponent()) {
-                        append(it)
-                    }
-                    appendln()
-                }
-
-                append("-----------------------------------", Color.DARK_GRAY)
-            }
-        }
+        private var commandDefault: CommandDefault = CommandDefault.Builder().build()
 
         fun completion(commandCompletion: CommandCompletion): Builder {
             this.commandCompletion = commandCompletion
@@ -239,10 +108,20 @@ class CommandHandler(
             return this
         }
 
+        fun default(commandDefault: CommandDefault): Builder {
+            this.commandDefault = commandDefault
+            return this
+        }
+
+        fun default(init: CommandDefault.Builder.() -> Unit): Builder {
+            commandDefault = CommandDefault.Builder().apply(init).build()
+            return this
+        }
+
         /**
          * Registers the specified command. It is not necessary to register commands or permissions in plugin.yml.
          */
-        fun registerCommand(command: Command): Builder {
+        fun register(command: Command): Builder {
             fun List<Command>.setParent(parent: Command): Unit = forEach {
                 it.parent = parent
                 it.children.setParent(it)
@@ -256,11 +135,7 @@ class CommandHandler(
         fun build() = CommandHandler(
             commands,
             commandCompletion,
-            defaultDescription,
-            defaultPermission,
-            defaultPlayerOnly,
-            invalidCommandMessage,
-            defaultSendHelp
+            commandDefault
         )
     }
 }
