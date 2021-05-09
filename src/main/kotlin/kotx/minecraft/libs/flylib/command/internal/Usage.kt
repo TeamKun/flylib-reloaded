8992ced7cbe8864d621e18e5eb5190ddef0c4ec9
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-10 03:49:30 +0900

    🔧 Update: of Argument

--- src/main/kotlin/kotx/minecraft/libs/flylib/command/internal/Usage.kt
+++ src/main/kotlin/kotx/minecraft/libs/flylib/command/internal/Usage.kt
@@ -5,44 +5,8 @@
 
 package kotx.minecraft.libs.flylib.command.internal
 
-/**
- * This is how to use commands.
- * ## Examples
- * Consider the case where there is a command called BanCommand.
- * Add the following to BanCommand.
- * ```kotlin
- * usages = listOf(
- *     Usage(
- *         context = "ban <user>",
- *         description = "ban a user"
- *     )
- * )
- * ```
- *
- * Consider the case where there is a command called BlackListCommand, and AddCommand and RemoveCommand exist as child elements of it.
- * If BlackListCommand does not take any arguments other than subcommands, the usages of BlackListCommand should be empty.
- * Instead, add the following to AddCommand.
- * ```kotlin
- * usages = listOf(
- *     Usage(
- *         context = "add <user>",
- *         description = "Add blacklist of user",
- *     )
- * )
- * ```
- * Add the following to the RemoveCommand.
- * ```kotlin
- * usages = listOf(
- *     Usage(
- *         context = "add <user>",
- *         description = "Add blacklist of user",
- *     )
- * )
- * ```
- * By doing so, the RemoveCommand usages will not be displayed when using sendHelp for AddCommand, and the usages for both AddCommand and RemoveCommand will only be displayed when using sendHelp for BlackListCommand. usages for both AddCommand and RemoveCommand only when using sendHelp for BlackListCommand.
- */
-data class Usage(
-    val context: String,
+class Usage(
+    vararg val args: Argument,
     val description: String = "",
     val options: List<Option> = emptyList()
 )
