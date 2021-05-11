0bd55955a9a4689e8eedbe423eb97afae3b86544
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-11 20:07:19 +0900

    📚 Docs: created

--- src/main/kotlin/kotx/minecraft/libs/flylib/command/internal/Usage.kt
+++ src/main/kotlin/kotx/minecraft/libs/flylib/command/internal/Usage.kt
@@ -7,9 +7,26 @@ package kotx.minecraft.libs.flylib.command.internal
 
 import kotx.minecraft.libs.flylib.command.CommandContext
 
+/**
+ * A class that takes arguments.
+ * The default send Help refers to this Usage Argument to compose the message.
+ *
+ * Example:
+ * override val usages: List<Usage> = listOf(
+ *      Usage(
+ *          Argument.Text("input")
+ *          "Print user input",
+ *          Permission.OP
+ *      ) {
+ *          sendMessage("You typed: ${args.first()}")
+ *      }
+ * )
+ */
 class Usage(
     vararg val args: Argument,
     val description: String = "",
+    val permission: Permission? = null,
+    val playerOnly: Boolean? = null,
     val options: List<Option> = emptyList(),
     val action: (CommandContext.() -> Unit)? = null
 )
