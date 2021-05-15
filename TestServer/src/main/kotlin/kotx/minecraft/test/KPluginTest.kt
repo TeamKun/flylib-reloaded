2522cfc88a513199ee6405570e649167cd904e2f
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-16 01:38:00 +0900

    おいヤメルルォ！

--- TestServer/src/main/kotlin/kotx/minecraft/test/KPluginTest.kt
+++ TestServer/src/main/kotlin/kotx/minecraft/test/KPluginTest.kt
@@ -5,12 +5,60 @@
 
 package kotx.minecraft.test
 
+import kotx.minecraft.libs.flylib.command.Command
+import kotx.minecraft.libs.flylib.command.internal.Argument
+import kotx.minecraft.libs.flylib.command.internal.Usage
 import kotx.minecraft.libs.flylib.flyLib
 import org.bukkit.plugin.java.JavaPlugin
 
 class PluginTest : JavaPlugin() {
     override fun onEnable() {
         flyLib {
+            command {
+                defaultConfiguration {
+                    playerOnly(true)
+                }
+
+                register(PrintNumberCommand())
+                register(ExplodeCommand())
+                register(OuterCommand())
+            }
         }
+    }
+}
+
+class PrintNumberCommand : Command("printnumber") {
+    override val usages: List<Usage> = listOf(
+        Usage(Argument.Integer("number", max = 10)) {
+            sendMessage("Your Number -> ${args.first().toInt()}")
+        }
+    )
+}
+
+class ExplodeCommand : Command("explode") {
+    override val usages: List<Usage> = listOf(
+        Usage(
+            Argument.Selection("type", "here", "there")
+        ),
+        Usage(
+            Argument.Position("location")
+        ),
+        Usage(
+            Argument.Player("player")
+        ),
+    )
+}
+
+class OuterCommand : Command("outer") {
+    override val children: List<Command> = listOf(
+        InnerCommand()
+    )
+
+    class InnerCommand : Command("inner") {
+        override val usages: List<Usage> = listOf(
+            Usage(
+                Argument.Selection("hoge/fuga", "hoge", "fuga")
+            )
+        )
     }
 }
