4e9646285d94530d677209bad1ffd8e40fe5d704
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-14 03:38:25 +0900

    new ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 val projectName = "flylib-reloaded"
 val projectGroup = "dev.kotx"
-val projectVersion = "0.1.17"
+val projectVersion = "0.1.18"
 
 group = projectGroup
 version = projectVersion
