4e0893eb35ca68bcac84412ece6a9917e25c84d1
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-14 20:03:09 +0900

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,20 @@
  * Twitter: https://twitter.com/kotx__
  */
 
-import com.github.jengelman.gradle.plugins.shadow.ShadowExtension
+import com.github.jengelman.gradle.plugins.shadow.*
+import com.github.jengelman.gradle.plugins.shadow.tasks.*
 
 plugins {
     kotlin("jvm") version "1.5.10"
     id("com.github.johnrengelman.shadow") version "5.2.0"
     java
     `maven-publish`
+    signing
 }
 
 val projectName = "flylib-reloaded"
 val projectGroup = "dev.kotx"
-val projectVersion = "0.1.21"
+val projectVersion = "0.1.22"
 
 group = projectGroup
 version = projectVersion
@@ -47,26 +49,70 @@ tasks {
         options.encoding = "UTF-8"
     }
 
+    val relocateShadow by registering(ConfigureShadowRelocation::class) {
+        target = shadowJar.get()
+        prefix = "dev.kotx"
+    }
+
     shadowJar {
         archiveBaseName.set(projectName)
         archiveClassifier.set("")
-        relocate("kotlin", "dev.kotx.kotlin")
-        relocate("org.koin", "dev.kotx.koin")
-        relocate("ch.qos.logback", "dev.kotx.logback")
+
+        dependsOn(relocateShadow)
     }
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                configure<ShadowExtension> {
-                    component(this@create)
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            configure<ShadowExtension> {
+                component(this@create)
+            }
+            artifactId = projectName
+            groupId = projectGroup
+            version = projectVersion
+
+            pom {
+                name.set(projectName)
+                description.set("a utility library for Minecraft Paper that provides commands, menus, Kotlin extensions, and more.")
+                url.set("https://github.com/TeamKun/flylib-reloaded")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/mit-license.php")
+                    }
                 }
-                artifactId = projectName
-                groupId = projectGroup
-                version = projectVersion
+
+                developers {
+                    developer {
+                        id.set("kotx__")
+                        name.set("kotlin chan")
+                        email.set("developer.hazuku@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("https://github.com/TeamKun/flylib-reloaded.git")
+                    developerConnection.set("https://github.com/Kotlin-chan")
+                    url.set("https://github.com/TeamKun/flylib-reloaded")
+                }
             }
         }
     }
+
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            val credentialFile = file("./.credential")
+            val credential = credentialFile.readText().split("\n")
+            credentials {
+                username = credential[0]
+                password = credential[1]
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["release"])
 }
