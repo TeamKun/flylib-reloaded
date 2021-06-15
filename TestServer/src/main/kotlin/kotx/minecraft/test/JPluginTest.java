d1a1a253bf53cef8c10dc82ebc35a8fb327b8b4d
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-16 02:05:42 +0900

    test

--- TestServer/src/main/kotlin/kotx/minecraft/test/JPluginTest.java
+++ TestServer/src/main/kotlin/kotx/minecraft/test/JPluginTest.java
@@ -7,21 +7,40 @@ package kotx.minecraft.test;
 
 import dev.kotx.flylib.FlyLib;
 import dev.kotx.flylib.command.Command;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public class JPluginTest extends JavaPlugin {
     @Override
     public void onEnable() {
-        FlyLib.inject(this, flyLib -> {
-            flyLib.command(commandHandler -> {
-                commandHandler.register(new TestCommand());
-            });
-        });
+        FlyLib.inject(this, flylib -> flylib.command(command -> command.register(new ExplodeCommand())));
     }
 }
 
-class TestCommand extends Command {
-    public TestCommand() {
-        super("test");
+class ExplodeCommand extends Command {
+    public ExplodeCommand() {
+        super("explode");
+        addUsage(builder -> {
+            builder.floatArgument("power", 10, 20, ctx -> {
+                ctx.addSuggestion("15");
+            });
+            builder.playerArgument("targets");
+
+            builder.executes(context -> {
+                float power = (float) context.getTypedArgs()[0];
+                List<Player> targets = (List<Player>) context.getTypedArgs()[1];
+                targets.forEach(player -> {
+                    context.getWorld().createExplosion(player, power);
+                });
+            });
+        });
+        addExample("explode 15 @a");
+    }
+
+    @Override
+    public String getDescription() {
+        return "Explode someone.";
     }
 }
