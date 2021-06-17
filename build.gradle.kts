9af7005a47d43c43737241a73dedc439604a343b
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-17 23:39:31 +0900

    rename

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 val projectName = "flylib-reloaded"
 val projectGroup = "dev.kotx"
-val projectVersion = "0.2.0"
+val projectVersion = "0.2.1"
 
 group = projectGroup
 version = projectVersion
