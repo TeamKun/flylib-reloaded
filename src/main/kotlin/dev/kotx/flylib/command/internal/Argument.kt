ad35fbfe934b84082fb9c583cc934749f484b60e
Kotlin-Chan <developer.hazuku@gmail.com>
2021-06-17 22:17:44 +0900

    optimize imports, move action into inner class

--- src/main/kotlin/dev/kotx/flylib/command/internal/Argument.kt
+++ src/main/kotlin/dev/kotx/flylib/command/internal/Argument.kt
@@ -7,7 +7,6 @@ package dev.kotx.flylib.command.internal
 
 import com.mojang.authlib.*
 import com.mojang.brigadier.arguments.*
-import dev.kotx.flylib.command.*
 import net.minecraft.server.v1_16_R3.*
 import java.util.function.*
 
@@ -17,11 +16,11 @@ sealed class Argument<T>(
     val name: String,
     val type: ArgumentType<*>? = null,
     val parser: (Context, String) -> T,
-    val tabComplete: CompletionAction? = null,
+    val tabComplete: Action? = null,
 ) {
     class Anchor @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<ArgumentAnchor.Anchor>(
         name,
         ArgumentAnchor.a(),
@@ -30,7 +29,7 @@ sealed class Argument<T>(
     )
 
     class Angle @JvmOverloads constructor(
-        name: String, tabComplete: CompletionAction? = null,
+        name: String, tabComplete: Action? = null,
     ) : Argument<kotlin.Float>(
         name,
         ArgumentAngle.a(), { ctx: Context, key: String -> ArgumentAngle.a(ctx, key) },
@@ -38,7 +37,7 @@ sealed class Argument<T>(
     )
 
     class Block @JvmOverloads constructor(
-        name: String, tabComplete: CompletionAction? = null,
+        name: String, tabComplete: Action? = null,
     ) : Argument<Predicate<ShapeDetectorBlock>>(
         name,
         ArgumentBlockPredicate.a(), { ctx: Context, key: String -> ArgumentBlockPredicate.a(ctx, key) },
@@ -47,7 +46,7 @@ sealed class Argument<T>(
 
     class Chat @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<IChatBaseComponent>(
         name,
         ArgumentChat.a(),
@@ -57,7 +56,7 @@ sealed class Argument<T>(
 
     class ChatComponent @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<IChatBaseComponent>(
         name,
         ArgumentChatComponent.a(),
@@ -67,7 +66,7 @@ sealed class Argument<T>(
 
     class Entity @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<net.minecraft.server.v1_16_R3.Entity>(
         name,
         ArgumentEntity.multipleEntities(),
@@ -77,91 +76,91 @@ sealed class Argument<T>(
 
     class EntityName @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<MinecraftKey>(name, ArgumentEntitySummon.a(), { ctx: Context, key: String ->
         ArgumentEntitySummon.a(ctx, key)
     }, tabComplete)
 
     class Item @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<ArgumentPredicateItemStack>(name, ArgumentItemStack.a(), { ctx: Context, key: String ->
         ArgumentItemStack.a(ctx, key)
     }, tabComplete)
 
     class Math @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<ArgumentMathOperation.a>(name, ArgumentMathOperation.a(), { ctx: Context, key: String ->
         ArgumentMathOperation.a(ctx, key)
     }, tabComplete)
 
     class MobEffect @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<MobEffectList>(name, ArgumentMobEffect.a(), { ctx: Context, key: String ->
         ArgumentMobEffect.a(ctx, key)
     }, tabComplete)
 
     class Particle @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<ParticleParam>(name, ArgumentParticle.a(), { ctx: Context, key: String ->
         ArgumentParticle.a(ctx, key)
     }, tabComplete)
 
     class Position @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<BlockPosition>(name, ArgumentPosition.a(), { ctx: Context, key: String ->
         ArgumentPosition.a(ctx, key)
     }, tabComplete)
 
     class Player @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<Collection<GameProfile>>(name, ArgumentProfile.a(), { ctx: Context, key: String ->
         ArgumentProfile.a(ctx, key)
     }, tabComplete)
 
     class Rotation @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<IVectorPosition>(name, ArgumentRotation.a(), { ctx: Context, key: String ->
         ArgumentRotation.a(ctx, key)
     }, tabComplete)
 
     class Vec2 @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<Vec2F>(name, ArgumentVec2.a(), { ctx: Context, key: String ->
         ArgumentVec2.a(ctx, key)
     }, tabComplete)
 
     class Vec3 @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<Vec3D>(name, ArgumentVec3.a(), { ctx: Context, key: String ->
         ArgumentVec3.a(ctx, key)
     }, tabComplete)
 
     class UUID @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<java.util.UUID>(name, ArgumentUUID.a(), { ctx: Context, key: String ->
         ArgumentUUID.a(ctx, key)
     }, tabComplete)
 
     class Dimension @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<WorldServer>(name, ArgumentDimension.a(), { ctx: Context, key: String ->
         ArgumentDimension.a(ctx, key)
     }, tabComplete)
 
     class Enchantment @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<net.minecraft.server.v1_16_R3.Enchantment>(
         name,
         ArgumentEnchantment.a(),
@@ -174,7 +173,7 @@ sealed class Argument<T>(
     class Text @JvmOverloads constructor(
         name: String,
         type: StringType = StringType.WORD,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<String>(
         name, when (type) {
             StringType.WORD -> StringArgumentType.word()
@@ -202,7 +201,7 @@ sealed class Argument<T>(
         name: String,
         min: Int = Int.MIN_VALUE,
         max: Int = Int.MAX_VALUE,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<Int>(
         name, IntegerArgumentType.integer(min, max), { ctx: Context, key: String ->
             IntegerArgumentType.getInteger(ctx, key)
@@ -213,7 +212,7 @@ sealed class Argument<T>(
         name: String,
         min: kotlin.Float = kotlin.Float.MIN_VALUE,
         max: kotlin.Float = kotlin.Float.MAX_VALUE,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<kotlin.Float>(name, FloatArgumentType.floatArg(min, max), { ctx: Context, key: String ->
         FloatArgumentType.getFloat(ctx, key)
     }, tabComplete)
@@ -222,7 +221,7 @@ sealed class Argument<T>(
         name: String,
         min: kotlin.Double = kotlin.Double.MIN_VALUE,
         max: kotlin.Double = kotlin.Double.MAX_VALUE,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<kotlin.Double>(name, DoubleArgumentType.doubleArg(min, max), { ctx: Context, key: String ->
         DoubleArgumentType.getDouble(ctx, key)
     }, tabComplete)
@@ -231,19 +230,19 @@ sealed class Argument<T>(
         name: String,
         min: kotlin.Long = kotlin.Long.MIN_VALUE,
         max: kotlin.Long = kotlin.Long.MAX_VALUE,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<kotlin.Long>(name, LongArgumentType.longArg(min, max), { ctx: Context, key: String ->
         LongArgumentType.getLong(ctx, key)
     }, tabComplete)
 
     class Boolean @JvmOverloads constructor(
         name: String,
-        tabComplete: CompletionAction? = null,
+        tabComplete: Action? = null,
     ) : Argument<kotlin.Boolean>(name, BoolArgumentType.bool(), { ctx: Context, key: String ->
         BoolArgumentType.getBool(ctx, key)
     }, tabComplete)
-}
 
-fun interface CompletionAction {
-    fun SuggestionBuilder.complete()
+    fun interface Action {
+        fun Suggestion.Builder.complete()
+    }
 }
