f5a5c3e7371c0a1b5d7ff84717ea11cf2a6fb3e9
kotx <developer.hazuku@gmail.com>
2021-06-23 02:38:57 +0900

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 val projectName = "flylib-reloaded"
 val projectGroup = "dev.kotx"
-val projectVersion = "0.2.21"
+val projectVersion = "0.2.22"
 
 group = projectGroup
 version = projectVersion
