d1344197285ae4ca1ef08a686f06c6e99b03e6d6
kotx <developer.hazuku@gmail.com>
2021-06-27 21:34:15 +0900

    分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて分からん助けて

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 val projectName = "flylib-reloaded"
 val projectGroup = "dev.kotx"
-val projectVersion = "0.2.32"
+val projectVersion = "0.2.33"
 
 group = projectGroup
 version = projectVersion
