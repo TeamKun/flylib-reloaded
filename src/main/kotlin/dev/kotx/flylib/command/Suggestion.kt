356830b3fbe0774e8695f725962768067be4a3ff
Kotx <developer.hazuku@gmail.com>
2021-07-08 02:45:56 +0900

    new Suggestions

---
+++ src/main/kotlin/dev/kotx/flylib/command/Suggestion.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.command
+
+internal class Suggestion(
+    internal val content: String,
+    internal val tooltip: String? = null
+)
