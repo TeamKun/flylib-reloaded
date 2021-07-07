ed89c654711058fb7dabcad840ca08cba3f18d84
Kotx <developer.hazuku@gmail.com>
2021-07-08 02:46:41 +0900

    new StringArgument.Type wrapper

---
+++ src/main/kotlin/dev/kotx/flylib/command/arguments/TextArgumentType.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.command.arguments
+
+enum class TextArgumentType {
+    WORD,
+    PHRASE_QUOTED,
+    PHRASE
+}
