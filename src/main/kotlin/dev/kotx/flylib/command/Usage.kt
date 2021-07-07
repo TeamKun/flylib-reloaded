d7667e46783a08c7da6b3017a9f12f5e50a73b08
Kotx <developer.hazuku@gmail.com>
2021-07-08 02:46:50 +0900

    new Usage

---
+++ src/main/kotlin/dev/kotx/flylib/command/Usage.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.command
+
+import org.bukkit.permissions.*
+
+internal class Usage(
+    val arguments: List<Argument<*>>,
+    val description: String? = null,
+    val permission: Permission? = null,
+    val action: (() -> Unit)? = null
+)
