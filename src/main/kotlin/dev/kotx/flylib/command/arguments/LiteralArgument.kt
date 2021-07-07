5b5db70e66652deacad627db9e0b93ff23f71cc0
Kotx <developer.hazuku@gmail.com>
2021-07-08 02:45:34 +0900

    new Argument

---
+++ src/main/kotlin/dev/kotx/flylib/command/arguments/LiteralArgument.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.command.arguments
+
+import dev.kotx.flylib.command.*
+
+internal class LiteralArgument(name: String) : Argument<String>(name, {})
