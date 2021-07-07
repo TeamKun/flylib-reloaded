f8de5562b9616d865bab5f5ce764cf9186a77a75
Kotx <developer.hazuku@gmail.com>
2021-07-08 03:10:08 +0900

    test

---
+++ TestServer/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+import dev.kotx.flylib.command.Command;
+
+public class Hello extends Command {
+    public Hello() {
+        super("");
+    }
+}
