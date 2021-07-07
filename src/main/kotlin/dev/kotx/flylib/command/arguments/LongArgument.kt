067be9c2ddc2d9c149342b2539217db9f7a97d53
Kotx <developer.hazuku@gmail.com>
2021-07-08 02:45:47 +0900

    new LongArg

---
+++ src/main/kotlin/dev/kotx/flylib/command/arguments/LongArgument.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.command.arguments
+
+import com.mojang.brigadier.arguments.*
+import com.mojang.brigadier.context.*
+import dev.kotx.flylib.command.*
+import net.minecraft.server.v1_16_R3.*
+
+internal class LongArgument(
+    name: String,
+    min: Long = Long.MIN_VALUE,
+    max: Long = Long.MAX_VALUE,
+    suggestion: SuggestionBuilder.() -> Unit = {}
+) : Argument<Long>(name, suggestion) {
+    override val type: ArgumentType<*>? = LongArgumentType.longArg(min, max)
+
+    override fun parse(context: CommandContext<CommandListenerWrapper>, key: String): Long = LongArgumentType.getLong(context, key)
+}
