5770dedfe9785b6ec5e3f0f5946684dd8ee2b8f9
Kotx <developer.hazuku@gmail.com>
2021-07-09 03:32:43 +0900

    add context

--- src/main/kotlin/dev/kotx/flylib/command/UsageBuilder.kt
+++ src/main/kotlin/dev/kotx/flylib/command/UsageBuilder.kt
@@ -5,13 +5,11 @@
 
 package dev.kotx.flylib.command
 
-import org.bukkit.permissions.*
-
 class UsageBuilder {
     private val arguments = mutableListOf<Argument<*>>()
     private var description: String? = null
     private var permission: Permission? = null
-    private var action: (() -> Unit)? = null
+    private var action: ((CommandContext) -> Unit)? = null
 
     internal fun build() = Usage(arguments, description, permission, action)
 }
