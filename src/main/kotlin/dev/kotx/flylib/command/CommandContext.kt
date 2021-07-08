62f120d62778381df12c9e7938d9d42d0e8527eb
Kotx <developer.hazuku@gmail.com>
2021-07-09 03:30:08 +0900

    new CommandContext

---
+++ src/main/kotlin/dev/kotx/flylib/command/CommandContext.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.command
+
+import org.bukkit.*
+import org.bukkit.command.*
+import org.bukkit.entity.*
+import org.bukkit.plugin.java.*
+
+class CommandContext(
+    val plugin: JavaPlugin,
+    val command: Command,
+    val sender: CommandSender,
+    val world: World?,
+    val message: String,
+) {
+    val player = sender as? Player
+    val args = message.replaceFirst("^/".toRegex(), "").split(" ")
+}
