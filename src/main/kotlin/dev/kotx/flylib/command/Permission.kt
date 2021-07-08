eee5e70d2a9189a42a0dd0901093dc8aebfe766a
Kotx <developer.hazuku@gmail.com>
2021-07-09 03:32:49 +0900

    new Permission.kt

---
+++ src/main/kotlin/dev/kotx/flylib/command/Permission.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.command
+
+import org.bukkit.permissions.*
+
+class Permission(
+    val name: String? = null,
+    val defaultPermission: PermissionDefault
+) {
+
+    companion object {
+        @JvmStatic
+        val OP = Permission(null, PermissionDefault.OP)
+
+        @JvmStatic
+        val EVERYONE = Permission(null, PermissionDefault.TRUE)
+    }
+}
