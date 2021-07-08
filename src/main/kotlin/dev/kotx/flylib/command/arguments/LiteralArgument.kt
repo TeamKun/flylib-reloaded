523c365e41ae12b12783b7580cd7c169dfe6daa5
Kotx <developer.hazuku@gmail.com>
2021-07-09 03:31:25 +0900

    nullabel

--- src/main/kotlin/dev/kotx/flylib/command/arguments/LiteralArgument.kt
+++ src/main/kotlin/dev/kotx/flylib/command/arguments/LiteralArgument.kt
@@ -6,13 +6,13 @@
 package dev.kotx.flylib.command.arguments
 
 import com.mojang.brigadier.arguments.*
-import com.mojang.brigadier.context.*
+import com.mojang.brigadier.context.CommandContext
 import dev.kotx.flylib.command.*
 import net.minecraft.server.v1_16_R3.*
 
 class LiteralArgument(override val name: String) : Argument<String> {
     override val type: ArgumentType<*>? = null
-    override val suggestion: SuggestionBuilder.() -> Unit = {}
+    override val suggestion: (SuggestionBuilder.() -> Unit)? = null
 
     override fun parse(context: CommandContext<CommandListenerWrapper>, key: String) = key
 }
