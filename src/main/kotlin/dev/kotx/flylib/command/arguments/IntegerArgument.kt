0d28f6d3e6ed6791059df2df4ad8c8bf603a1336
Kotx <developer.hazuku@gmail.com>
2021-07-09 03:31:16 +0900

    nullable

--- src/main/kotlin/dev/kotx/flylib/command/arguments/IntegerArgument.kt
+++ src/main/kotlin/dev/kotx/flylib/command/arguments/IntegerArgument.kt
@@ -6,7 +6,7 @@
 package dev.kotx.flylib.command.arguments
 
 import com.mojang.brigadier.arguments.*
-import com.mojang.brigadier.context.*
+import com.mojang.brigadier.context.CommandContext
 import dev.kotx.flylib.command.*
 import net.minecraft.server.v1_16_R3.*
 
@@ -14,7 +14,7 @@ class IntegerArgument(
     override val name: String,
     min: Int = Int.MIN_VALUE,
     max: Int = Int.MAX_VALUE,
-    override val suggestion: SuggestionBuilder.() -> Unit = {}
+    override val suggestion: (SuggestionBuilder.() -> Unit)? = null
 ) : Argument<Int> {
     override val type: ArgumentType<*>? = IntegerArgumentType.integer(min, max)
 
