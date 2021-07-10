7afaae3ef02e500d5ef806b91e481f160eda8841
Kotx <developer.hazuku@gmail.com>
2021-07-11 00:22:11 +0900

    documented

--- src/main/kotlin/dev/kotx/flylib/command/Usage.kt
+++ src/main/kotlin/dev/kotx/flylib/command/Usage.kt
@@ -5,9 +5,24 @@
 
 package dev.kotx.flylib.command
 
+/**
+ * Command usage and definition
+ */
 class Usage(
+        /**
+         * A list of arguments. This is also used for the definition.
+         */
         val arguments: List<Argument<*>>,
+        /**
+         * Explanation of usage and definition. Used in the default help message.
+         */
         val description: String? = null,
+        /**
+         * The privileges required to execute this definition. The default is specified by the FlyLibBuilder defaultPermission.
+         */
         val permission: Permission? = null,
+        /**
+         * What will be executed if you enter this definition.
+         */
         val action: ContextAction? = null
 )
