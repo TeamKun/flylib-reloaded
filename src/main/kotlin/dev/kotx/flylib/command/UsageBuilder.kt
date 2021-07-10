02a2b4ee7c948bf883ab11152646e318e0dbbd87
Kotx <developer.hazuku@gmail.com>
2021-07-11 00:03:11 +0900

    add overlords

--- src/main/kotlin/dev/kotx/flylib/command/UsageBuilder.kt
+++ src/main/kotlin/dev/kotx/flylib/command/UsageBuilder.kt
@@ -39,31 +39,52 @@ class UsageBuilder {
         return this
     }
 
+    @JvmOverloads
     fun integerArgument(name: String, min: Int = Int.MIN_VALUE, max: Int = Int.MAX_VALUE, suggestion: SuggestionAction? = null): UsageBuilder {
         this.arguments.add(IntegerArgument(name, min, max, suggestion))
         return this
     }
 
+    fun integerArgument(name: String, suggestion: SuggestionAction? = null): UsageBuilder {
+        this.arguments.add(IntegerArgument(name, suggestion = suggestion))
+        return this
+    }
+
+    @JvmOverloads
     fun longArgument(name: String, min: Long = Long.MIN_VALUE, max: Long = Long.MAX_VALUE, suggestion: SuggestionAction? = null): UsageBuilder {
         this.arguments.add(LongArgument(name, min, max, suggestion))
         return this
     }
 
+    fun longArgument(name: String, suggestion: SuggestionAction? = null): UsageBuilder {
+        this.arguments.add(IntegerArgument(name, suggestion = suggestion))
+        return this
+    }
+
+    @JvmOverloads
     fun textArgument(name: String, type: TextArgument.Type = TextArgument.Type.WORD, suggestion: SuggestionAction? = null): UsageBuilder {
         this.arguments.add(TextArgument(name, type, suggestion))
         return this
     }
 
+    fun textArgument(name: String, suggestion: SuggestionAction? = null): UsageBuilder {
+        this.arguments.add(IntegerArgument(name, suggestion = suggestion))
+        return this
+    }
+
+    @JvmOverloads
     fun entityArgument(name: String, suggestion: SuggestionAction? = null): UsageBuilder {
         this.arguments.add(EntityArgument(name, suggestion))
         return this
     }
 
+    @JvmOverloads
     fun locationArgument(name: String, suggestion: SuggestionAction? = null): UsageBuilder {
         this.arguments.add(LocationArgument(name, suggestion))
         return this
     }
 
+    @JvmOverloads
     fun vectorArgument(name: String, suggestion: SuggestionAction? = null): UsageBuilder {
         this.arguments.add(VectorArgument(name, suggestion))
         return this
