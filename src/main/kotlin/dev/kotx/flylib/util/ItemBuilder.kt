4dd24aa408963edfa0f4807e3a0b3b27a76cbac9
Kotx <developer.hazuku@gmail.com>
2021-07-11 01:04:19 +0900

    documented

--- src/main/kotlin/dev/kotx/flylib/util/ItemBuilder.kt
+++ src/main/kotlin/dev/kotx/flylib/util/ItemBuilder.kt
@@ -12,6 +12,9 @@ import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
+/**
+ * Item builder to create custom itemStack
+ */
 class ItemBuilder(
         private val material: Material
 ) {
@@ -23,46 +26,73 @@ class ItemBuilder(
     private val flags = mutableListOf<ItemFlag>()
     private val enchants = mutableMapOf<Enchantment, Int>()
 
+    /**
+     * Sets item amount.
+     */
     fun amount(amount: Int): ItemBuilder {
         this.amount = amount
         return this
     }
 
+    /**
+     * Sets custom item name.
+     */
     fun name(name: String): ItemBuilder {
         this.name = name.component()
         return this
     }
 
+    /**
+     * Sets custom item name with component builder.
+     */
     fun name(builder: ComponentBuilder.() -> Unit): ItemBuilder {
         this.name = ComponentBuilder().apply(builder).build()
         return this
     }
 
+    /**
+     * Adds item lore.
+     */
     fun lore(vararg lore: String): ItemBuilder {
         this.lores.addAll(lore.map { it.component() })
         return this
     }
 
+    /**
+     * Adds item lore with component.
+     */
     fun lore(vararg lore: Component): ItemBuilder {
         this.lores.addAll(lore)
         return this
     }
 
+    /**
+     * Sets item meta.
+     */
     fun meta(metaBuilder: MetaBuilder): ItemBuilder {
         this.meta = metaBuilder
         return this
     }
 
+    /**
+     * Adds item flags.
+     */
     fun flag(vararg flag: ItemFlag): ItemBuilder {
         this.flags.addAll(flag)
         return this
     }
 
+    /**
+     * Enchant items.
+     */
     fun enchant(enchantment: Enchantment, level: Int): ItemBuilder {
         this.enchants[enchantment] = level
         return this
     }
 
+    /**
+     * Enchant items.
+     */
     fun enchant(vararg enchantment: Enchantment): ItemBuilder {
         this.enchants.putAll(enchantment.map { it to 1 })
         return this
