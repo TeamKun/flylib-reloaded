c35e9873c984453155f2171747ef087d26a6a194
Kotx <developer.hazuku@gmail.com>
2021-07-10 18:34:58 +0900

    create

---
+++ src/main/kotlin/dev/kotx/flylib/menu/BasicMenu.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 kotx__.
+ * Twitter: https://twitter.com/kotx__
+ */
+
+package dev.kotx.flylib.menu
+
+import org.bukkit.inventory.*
+
+class BasicMenu(
+    val size: Int,
+    val items: List<MenuItem>
+) {
+
+}
+
+class BasicMenuBuilder {
+
+}
+
+class MenuItem(index: Int, item: ItemStack)
