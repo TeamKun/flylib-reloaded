70a784da7e2d12d58d8af106fabd95f30a585c4d
Kotx <developer.hazuku@gmail.com>
2021-07-10 20:59:03 +0900

    wip

--- src/main/kotlin/dev/kotx/flylib/menu/BasicMenu.kt
+++ src/main/kotlin/dev/kotx/flylib/menu/BasicMenu.kt
@@ -5,17 +5,62 @@
 
 package dev.kotx.flylib.menu
 
+import dev.kotx.flylib.*
+import org.bukkit.*
+import org.bukkit.entity.*
+import org.bukkit.event.inventory.*
 import org.bukkit.inventory.*
+import java.awt.*
 
 class BasicMenu(
+    val title: String,
     val size: Int,
     val items: List<MenuItem>
-) {
+) : FlyLibComponent {
+    private val players = mutableMapOf<Player, Inventory>()
 
+    fun display(player: Player) {
+        val inventory = Bukkit.createInventory(player, size, title)
+        player.openInventory(inventory)!!
+
+        players[player] = inventory
+
+    }
+
+    fun close() {
+        players.forEach { (player, inventory) ->
+            player.closeInventory()
+        }
+
+        players.clear()
+    }
+
+    fun close(player: Player) {
+        players.remove(player)
+        player.closeInventory()
+    }
 }
 
 class BasicMenuBuilder {
+    private var title = "Chest"
+    private var size = 27
+    private val items = mutableListOf<MenuItem>()
 
+    fun title(title: String): BasicMenuBuilder {
+        this.title = title
+        return this
+    }
+
+    fun size(size: Int): BasicMenuBuilder {
+        this.size = size
+        return this
+    }
+
+    fun type(type: InventoryType): BasicMenuBuilder {
+        this.title = (type.defaultTitle() as TextComponent).text
+        this.size = type.defaultSize
+        return this
+    }
 }
 
 class MenuItem(index: Int, item: ItemStack)
