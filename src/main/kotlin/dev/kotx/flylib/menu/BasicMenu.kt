92ec5d6067f65b395aa4b80857005c8b403ada00
Kotx <developer.hazuku@gmail.com>
2021-07-11 00:34:07 +0900

    wip

--- src/main/kotlin/dev/kotx/flylib/menu/BasicMenu.kt
+++ src/main/kotlin/dev/kotx/flylib/menu/BasicMenu.kt
@@ -8,18 +8,35 @@ package dev.kotx.flylib.menu
 import dev.kotx.flylib.FlyLibComponent
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+import org.koin.core.component.inject
+import java.awt.MenuItem
 import java.awt.TextComponent
 
+/**
+ * A Inventory menu.
+ */
 class BasicMenu(
         val title: String,
         val size: Int,
         val items: List<MenuItem>
-) : FlyLibComponent {
+) : FlyLibComponent, Listener {
+    private val plugin: JavaPlugin by inject()
     private val players = mutableMapOf<Player, Inventory>()
 
+    init {
+        plugin.server.pluginManager.registerEvents(this, plugin)
+    }
+
+    /**
+     * Show the inventory to the player.
+     */
     fun display(player: Player) {
         val inventory = Bukkit.createInventory(player, size, title)
         player.openInventory(inventory)!!
@@ -27,6 +44,9 @@ class BasicMenu(
         players[player] = inventory
     }
 
+    /**
+     * Close the menus of all players displaying this menu.
+     */
     fun close() {
         players.forEach { (player, inventory) ->
             player.closeInventory()
@@ -35,11 +55,9 @@ class BasicMenu(
         players.clear()
     }
 
-    fun close(player: Player) {
-        players.remove(player)
-        player.closeInventory()
-    }
-
+    /**
+     *  Updates the menus of all players displaying this menu.
+     */
     fun update(builder: BasicMenuAction) {
         val menu = BasicMenuBuilder().apply { builder.apply { initialize() } }.build()
         val playerList = players.keys.toList()
@@ -48,9 +66,23 @@ class BasicMenu(
         playerList.forEach { menu.display(it) }
     }
 
+    @EventHandler
+    fun onInventoryClose(event: InventoryCloseEvent) {
+        val playerList = players.filter { it.value == event.inventory }.keys.toList()
+        playerList.forEach {
+            players.remove(it)
+        }
+    }
+
     companion object {
+        /**
+         * Create a menu using the builder.
+         */
         fun create(builder: BasicMenuAction) = BasicMenuBuilder().apply { builder.apply { initialize() } }.build()
 
+        /**
+         * Displays the menu of the builder specified by the player.
+         */
         fun display(player: Player, builder: BasicMenuAction) = create(builder).display(player)
     }
 }
