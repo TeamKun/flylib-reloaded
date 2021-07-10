c94d63fc92335d93fdaef08650481d00376ec529
Kotx <developer.hazuku@gmail.com>
2021-07-11 00:15:50 +0900

    documented

--- src/main/kotlin/dev/kotx/flylib/command/Permission.kt
+++ src/main/kotlin/dev/kotx/flylib/command/Permission.kt
@@ -7,15 +7,32 @@ package dev.kotx.flylib.command
 
 import org.bukkit.permissions.PermissionDefault
 
+/**
+ * Command permissions.
+ */
 class Permission(
+        /**
+         * Custom name for permissions. If nothing is specified (if null), permissions will be automatically added according to the command name or Usage argument name.
+         */
         val name: String? = null,
+        /**
+         * Permission request line. Check the following for details.
+         *
+         * @see PermissionDefault
+         */
         val defaultPermission: PermissionDefault
 ) {
 
     companion object {
+        /**
+         * Permission that only OP can execute.
+         */
         @JvmStatic
         val OP = Permission(null, PermissionDefault.OP)
 
+        /**
+         * Privileges that everyone can do.
+         */
         @JvmStatic
         val EVERYONE = Permission(null, PermissionDefault.TRUE)
     }
