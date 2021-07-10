5300c3c8fd70f3f666c73f8abf00d1128d79896f
Kotx <developer.hazuku@gmail.com>
2021-07-11 00:13:26 +0900

    documented

--- src/main/kotlin/dev/kotx/flylib/command/CommandContext.kt
+++ src/main/kotlin/dev/kotx/flylib/command/CommandContext.kt
@@ -11,14 +11,49 @@ import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class CommandContext(
+        /**
+         * Your plugin.
+         */
         val plugin: JavaPlugin,
+        /**
+         * Executed command.
+         */
         val command: Command,
+        /**
+         * Executed command sender. (It doesn't matter if you are a player or not.)
+         */
         val sender: CommandSender,
+        /**
+         * The world where the command was executed
+         */
         val world: World?,
+        /**
+         * Command input message. The leading `` is deleted.
+         */
         val message: String,
         depth: Int
 ) {
+    /**
+     * Executed command sender. (Limited to the player, but null if executed by someone other than the player.)
+     */
     val player = sender as? Player
+
+    /**
+     * Command arguments. The remaining arguments except the beginning of the command are assigned.
+     *
+     * ## Example
+     * ### Basic command
+     * /command <number>
+     *     args: <number>
+     *
+     * ### Nested command
+     * /parent children <arg1> <arg2>
+     *     args: <arg1>, <arg2>
+     *
+     * ### Usage argument
+     * /command <literal> <text> <number> <entity>
+     *     args: <literal>, <text>, <number>, <entity>
+     */
     val args = message.replaceFirst("^/".toRegex(), "").split(" ").drop(depth)
 }
 
