e3e53d9dd70c103174ee9f9d01652dc77c44498a
Kotx <developer.hazuku@gmail.com>
2021-07-19 03:14:02 +0900

    add message functions

--- src/main/kotlin/dev/kotx/flylib/command/CommandContext.kt
+++ src/main/kotlin/dev/kotx/flylib/command/CommandContext.kt
@@ -5,10 +5,14 @@
 
 package dev.kotx.flylib.command
 
+import dev.kotx.flylib.util.ComponentBuilder
+import dev.kotx.flylib.util.ComponentBuilderAction
+import net.kyori.adventure.text.Component
 import org.bukkit.World
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
+import java.awt.Color
 
 /**
  * Command context.
@@ -60,6 +64,126 @@ class CommandContext(
      *     args: <literal>, <text>, <number>, <entity>
      */
     val args = message.replaceFirst("^/".toRegex(), "").split(" ").drop(depth)
+
+    /**
+     * Send message to command sender.
+     */
+    fun send(text: String) {
+        sender.sendMessage(text)
+    }
+
+    /**
+     * send message to command sender
+     */
+    fun send(component: Component) {
+        sender.sendMessage(component)
+    }
+
+    /**
+     * send message to command sender with ComponentBuilder
+     */
+    fun send(builder: ComponentBuilderAction) {
+        sender.sendMessage(ComponentBuilder().apply { builder.apply { initialize() } }.build())
+    }
+
+    /**
+     * send green colored message.
+     */
+    fun success(text: String) {
+        send { append(text, Color.GREEN) }
+    }
+
+    /**
+     * send yellow colored message.
+     */
+    fun warn(text: String) {
+        send { append(text, Color.YELLOW) }
+    }
+
+    /**
+     * send red colored message.
+     */
+    fun fail(text: String) {
+        send { append(text, Color.RED) }
+    }
+
+    /**
+     * send message with plugin name.
+     */
+    fun pluginSend(text: String) {
+        send {
+            append("[", Color.LIGHT_GRAY)
+            append(plugin.name, Color.ORANGE)
+            append("]", Color.LIGHT_GRAY)
+            append(" ")
+            append(text)
+        }
+    }
+
+    /**
+     * send message with plugin name.
+     */
+    fun pluginSend(component: Component) {
+        send {
+            append("[", Color.LIGHT_GRAY)
+            append(plugin.name, Color.ORANGE)
+            append("]", Color.LIGHT_GRAY)
+            append(" ")
+            append(component)
+        }
+    }
+
+    /**
+     * send message with plugin name using ComponentBuilder
+     */
+    fun pluginSend(builder: ComponentBuilderAction) {
+        send {
+            append("[", Color.LIGHT_GRAY)
+            append(plugin.name, Color.ORANGE)
+            append("]", Color.LIGHT_GRAY)
+            append(" ")
+            append(ComponentBuilder().apply { builder.apply { initialize() } }.build())
+        }
+    }
+
+    /**
+     * send green colored message with plugin name.
+     */
+    fun pluginSendSuccess(text: String) {
+        send {
+            append("[", Color.LIGHT_GRAY)
+            append(plugin.name, Color.ORANGE)
+            append("]", Color.LIGHT_GRAY)
+            append(" ")
+            append(text, Color.GREEN)
+        }
+    }
+
+    /**
+     * send yellow colored message with plugin name.
+     */
+    fun pluginSendWarn(text: String) {
+        send {
+            append("[", Color.LIGHT_GRAY)
+            append(plugin.name, Color.ORANGE)
+            append("]", Color.LIGHT_GRAY)
+            append(" ")
+            append(text, Color.YELLOW)
+        }
+    }
+
+    /**
+     * send red colored message with plugin name.
+     */
+    fun pluginSendError(text: String) {
+        send {
+            append("[", Color.LIGHT_GRAY)
+            append(plugin.name, Color.ORANGE)
+            append("]", Color.LIGHT_GRAY)
+            append(" ")
+            append(text, Color.RED)
+        }
+    }
 }
 
 /**
