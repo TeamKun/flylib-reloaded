0eb95c0d545cc4f4f6eb59deb36b8ac5071eea17
ことりん <34242962+Kotlin-chan@users.noreply.github.com>
2021-08-21 17:44:32 +0900

    test

--- test_plugin/src/main/kotlin/dev/kotx/test_plugin/TestPlugin.kt
+++ test_plugin/src/main/kotlin/dev/kotx/test_plugin/TestPlugin.kt
@@ -8,12 +8,17 @@ package dev.kotx.test_plugin
 import dev.kotx.flylib.command.Command
 import dev.kotx.flylib.command.CommandContext
 import dev.kotx.flylib.flyLib
+import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class TestPlugin : JavaPlugin() {
     init {
         flyLib {
             command(TestCommand())
+
+            listen(PlayerMoveEvent::class.java) { event ->
+                println("MOV")
+            }
         }
     }
 }
