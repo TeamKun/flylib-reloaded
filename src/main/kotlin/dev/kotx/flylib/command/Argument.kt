896342fd5d437105dcaed40e93cd030a8630d6fb
kotx <developer.hazuku@gmail.com>
2021-10-01 00:58:27 +0900

    add docs

--- src/main/kotlin/dev/kotx/flylib/command/Argument.kt
+++ src/main/kotlin/dev/kotx/flylib/command/Argument.kt
@@ -12,6 +12,10 @@ import net.minecraft.server.v1_16_R3.CommandListenerWrapper
 /**
  *  Command arguments. It can be used as a child element of Usage.
  *  There is a type in the argument, type analysis is automatically performed when the client inputs, and if it cannot be parsed, the command execution is refused.
+ *
+ *  @param name Name of argument.
+ *  @param suggestion Lambda expression for tab completion of its arguments.
+ *  @param type Mojang Brigadier argument type.
  */
 interface Argument<T> {
     val name: String
