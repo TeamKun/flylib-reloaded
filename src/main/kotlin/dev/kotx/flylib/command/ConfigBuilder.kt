8c95bddb504dde3ed1a8e790f306001c9a6e3425
kotx <developer.hazuku@gmail.com>
2021-11-15 18:23:50 +0900

    ♻️ Update: add array

--- src/main/kotlin/dev/kotx/flylib/command/ConfigBuilder.kt
+++ src/main/kotlin/dev/kotx/flylib/command/ConfigBuilder.kt
@@ -1,9 +1,14 @@
 package dev.kotx.flylib.command
 
+import dev.kotx.flylib.command.parameters.BooleanArrayParameter
 import dev.kotx.flylib.command.parameters.BooleanParameter
+import dev.kotx.flylib.command.parameters.DoubleArrayParameter
 import dev.kotx.flylib.command.parameters.DoubleParameter
+import dev.kotx.flylib.command.parameters.FloatArrayParameter
 import dev.kotx.flylib.command.parameters.FloatParameter
+import dev.kotx.flylib.command.parameters.IntegerArrayParameter
 import dev.kotx.flylib.command.parameters.IntegerParameter
+import dev.kotx.flylib.command.parameters.LongArrayParameter
 import dev.kotx.flylib.command.parameters.LongParameter
 import dev.kotx.flylib.command.parameters.ObjectParameter
 
@@ -22,6 +27,15 @@ class ConfigBuilder {
     }
 
     @JvmOverloads
+    fun integerArray(
+        key: String,
+        defaultValue: Array<Int>? = null,
+    ): ConfigBuilder {
+        parameters.add(IntegerArrayParameter(key, defaultValue))
+        return this
+    }
+
+    @JvmOverloads
     fun long(
         key: String,
         defaultValue: Long? = null,
@@ -29,6 +43,15 @@ class ConfigBuilder {
         max: Long = Long.MAX_VALUE
     ): ConfigBuilder {
         parameters.add(LongParameter(key, defaultValue, min, max))
+        return this
+    }
+
+    @JvmOverloads
+    fun longArray(
+        key: String,
+        defaultValue: Array<Long>? = null,
+    ): ConfigBuilder {
+        parameters.add(LongArrayParameter(key, defaultValue))
         return this
     }
 
@@ -44,6 +67,15 @@ class ConfigBuilder {
     }
 
     @JvmOverloads
+    fun floatArray(
+        key: String,
+        defaultValue: Array<Float>? = null,
+    ): ConfigBuilder {
+        parameters.add(FloatArrayParameter(key, defaultValue))
+        return this
+    }
+
+    @JvmOverloads
     fun double(
         key: String,
         defaultValue: Double? = null,
@@ -55,11 +87,29 @@ class ConfigBuilder {
     }
 
     @JvmOverloads
+    fun doubleArray(
+        key: String,
+        defaultValue: Array<Double>? = null,
+    ): ConfigBuilder {
+        parameters.add(DoubleArrayParameter(key, defaultValue))
+        return this
+    }
+
+    @JvmOverloads
     fun boolean(
         key: String,
         defaultValue: Boolean? = null
     ): ConfigBuilder {
         parameters.add(BooleanParameter(key, defaultValue))
+        return this
+    }
+
+    @JvmOverloads
+    fun booleanArray(
+        key: String,
+        defaultValue: Array<Boolean>? = null
+    ): ConfigBuilder {
+        parameters.add(BooleanArrayParameter(key, defaultValue))
         return this
     }
 
