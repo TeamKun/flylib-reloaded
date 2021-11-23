22336974a2138faab8774c584735c18062aa08ec
kotx <developer.hazuku@gmail.com>
2021-11-24 02:21:39 +0900

    🐛 Fix: bugs

--- src/main/kotlin/dev/kotx/flylib/command/elements/IntegerElement.kt
+++ src/main/kotlin/dev/kotx/flylib/command/elements/IntegerElement.kt
@@ -6,4 +6,5 @@ package dev.kotx.flylib.command.elements
 
 import dev.kotx.flylib.command.ConfigElement
 
-class IntegerElement(override val key: String, override var value: Int?, val min: Int, max: Int) : ConfigElement<Int>
+class IntegerElement(override val key: String, override var value: Int?, val min: Int, val max: Int) :
+    ConfigElement<Int>
